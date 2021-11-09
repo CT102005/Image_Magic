279bd8dd424f9cf013524a33ea8845aa1d25b3e6
Candice <candicetsai2005@gmail.com>
2021-11-09 09:05:39 -0800

    Completed boilerplate in pygame_drawing.py

--- pygame_drawing.py
+++ pygame_drawing.py
@@ -12,13 +12,38 @@ BLACK = (  0,   0,   0)
 RED   = (255,   0,   0)
 GREEN = (  0, 255,   0)
 BLUE  = (  0,   0, 255)
-
+LIGHT_BLUE  = (  0, 255, 255)
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+WINDOW_TITLE = "Pygame Drawing"
 
 def main() -> None:
     """Driver of the Python script"""
+    # Create the screen
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption(WINDOW_TITLE)
+
+    # Create some local variables that describe the environment
+    done = False
+    clock = pygame.time.Clock()
+
+    # ----------MAIN LOOP
+    while not done:
+        # Make space for the event listener
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        # ---------- CHANGE ENVIRONMENT
+        # ---------- DRAW THE ENVIRONMENT
+        screen.fill(WHITE)      # Fill with background colour
+        # Update the screen
+        pygame.display.flip()
+        # ----------CLOCK TICK
+        clock.tick(75)
+
 
 if __name__ == "__main__":
     main()
+
